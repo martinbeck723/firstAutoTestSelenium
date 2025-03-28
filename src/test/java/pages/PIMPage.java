dee83953b58940fc028a627f954a4310b5911f4e
martinbeck723 <36826752+martinbeck723@users.noreply.github.com>
2025-03-28 07:17:46 -0400

    add PIM page first step

---
+++ src/test/java/pages/PIMPage.java
@@ -0,0 +1,20 @@
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+public class PIMPage extends BasePage {
+
+    // Locator
+    private By addButton = By.xpath("//i[contains(@class, 'bi-plus')]/parent::button");
+
+    // Constructor
+    public PIMPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Method to click on add button
+    public void clickAddButton() {
+        click(addButton);
+    }
+}
---
+++ src/test/java/testrunner/AddPIMTest.java
@@ -0,0 +1,47 @@
+package testrunner;
+
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.*;
+import utils.ExtentManager;
+
+public class AddPIMTest extends BaseTest {
+    LoginPage loginPage;
+    DashboardPage dashboardPage;
+    PIMPage pimPage;
+
+    @Test
+    public void testAddAdmin() {
+        test = ExtentManager.createTest("Test Login Functionality");
+
+        //initialized before calling the pages
+        loginPage = new LoginPage(driver);
+        dashboardPage = new DashboardPage(driver);
+        pimPage = new PIMPage(driver);
+
+        logger.info("Entering login credentials...");
+        test.log(Status.INFO, "Entering login credentials.");
+
+        loginPage.enterUsername("Admin");
+        loginPage.enterPassword("admin123");
+        loginPage.clickLogin();
+
+        logger.info("Checking dashboard page.");
+        test.log(Status.INFO, "Checking if the user is on the dashboard page.");
+
+        // Verify successful login
+        String expectedText = "Dashboard";
+        String actualText = dashboardPage.getDashboardHeading();
+        Assert.assertEquals(actualText, expectedText, "Login failed!");
+
+        logger.info("Checking PIM page.");
+        test.log(Status.INFO, "Navigating to add PIM page.");
+        driver.findElement(By.linkText("PIM")).click();
+        pimPage.clickAddButton();
+
+        logger.info("On the add PIM page.");
+
+    }
+}
