f06ef2123496c15ea2d373eca73b00feec5947f2
martinbeck723 <36826752+martinbeck723@users.noreply.github.com>
2025-03-28 14:55:04 -0400

    add upload image and add login detail for new user

--- src/test/java/pages/PIMPage.java
+++ src/test/java/pages/PIMPage.java
@@ -7,10 +7,6 @@ public class PIMPage extends BasePage {
 
     // Locator
     private By addButton = By.xpath("//i[contains(@class, 'bi-plus')]/parent::button");
-    private By firstnameField = By.xpath("//input[contains(@name, 'firstName')]");
-    private By middlenameField = By.xpath("//input[contains(@name, 'middleName')]");
-    private By lastnameField = By.xpath("//input[contains(@name, 'lastName')]");
-    private By employeeIdField = By.xpath("//div[label[text()='Employee Id']]/following-sibling::div//input[contains(@class, 'oxd-input')]");
 
     // Constructor
     public PIMPage(WebDriver driver) {
@@ -20,24 +16,6 @@ public class PIMPage extends BasePage {
     // Method to click on add button
     public void clickAddButton() {
         click(addButton);
-    }
-
-    //enter text into firstname and last name
-    public void addName(String firstname, String lastname) {
-        sendKeysText(firstnameField, firstname);
-        sendKeysText(lastnameField, lastname);
-    }
-
-    //enter text into middle name
-    public void addMiddleName( String middlename) {
-        sendKeysText(middlenameField, middlename);
-    }
-
-
-
-    public void addEmployeeId(String id) {
-        //need to be unique
-        sendKeysText(employeeIdField, id);
     }
 
 
--- src/test/java/testrunner/AddPIMTest.java
+++ src/test/java/testrunner/AddPIMTest.java
@@ -7,19 +7,23 @@ import org.testng.annotations.Test;
 import pages.*;
 import utils.ExtentManager;
 
+import java.awt.*;
+
 public class AddPIMTest extends BaseTest {
     LoginPage loginPage;
     DashboardPage dashboardPage;
     PIMPage pimPage;
+    PIMAddUserPage pimAddUserPage;
 
     @Test
-    public void testAddAdmin() {
+    public void testAddAdmin() throws AWTException {
         test = ExtentManager.createTest("Test Login Functionality");
 
         //initialized before calling the pages
         loginPage = new LoginPage(driver);
         dashboardPage = new DashboardPage(driver);
         pimPage = new PIMPage(driver);
+        pimAddUserPage = new PIMAddUserPage(driver);
 
         logger.info("Entering login credentials...");
         test.log(Status.INFO, "Entering login credentials.");
@@ -42,8 +46,18 @@ public class AddPIMTest extends BaseTest {
         pimPage.clickAddButton();
 
         logger.info("On the add PIM page.");
-        pimPage.addName("Oscar","Liu");
-        pimPage.addEmployeeId("asd3");
+        pimAddUserPage.addName("Oscar","Liu");
+        pimAddUserPage.addEmployeeId("asd3");
+        pimAddUserPage.clickLoginDetails();
+
+        pimAddUserPage.textUsername("martin1222");
+
+        pimAddUserPage.textPassowrd("a1234567a");
+
+        pimAddUserPage.textConfirmPassowrd("a1234567a");
+        pimAddUserPage.clickAddImageButton();
+        pimAddUserPage.uploadFileWithRobot("C:\\Users\\liuyuan\\IdeaProjects\\firstAutoTestSelenium\\src\\test\\resources\\21-4.jpg");
+
 
     }
 }
